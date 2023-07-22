a67dbc7d4e9dd96f3ed0f06f381eb2c8362ec045
VanLocNguyen1999 <Locnv@selex.vn>
2023-07-22 17:55:49 +0700

    aaaaa

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -27,12 +27,10 @@
 #include "usart.h"
 #include "gpio.h"
 
-<<<<<<< HEAD
+
 #define FREQ_PWM_HZ	20000UL //20khz
 #define DEAD_TIME_NS	400 //400ns
-=======
 
->>>>>>> 0d6478ce6432d025df209c0aa6b5735b1b51b05b
 
 uint32_t adc_values[7];
 float throttle_adc = 0;
@@ -64,14 +62,13 @@ uint32_t duty = 0;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-<<<<<<< HEAD
+
 void set_deadtime_pwm_ns(uint32_t time_ns);
 void set_duty_pwm(float duty);
 void set_freq_pwm(uint32_t freq);
 float get_duty_value();
 void update_1khz(void);
-=======
->>>>>>> 0d6478ce6432d025df209c0aa6b5735b1b51b05b
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -125,11 +122,9 @@ int main(void)
   MX_TIM8_Init();
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
-<<<<<<< HEAD
-  update_1khz();
-=======
 
->>>>>>> 0d6478ce6432d025df209c0aa6b5735b1b51b05b
+  update_1khz();
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -143,7 +138,7 @@ int main(void)
   /* USER CODE END 3 */
 }
 
-<<<<<<< HEAD
+
  void TIM8_UP_IRQHandler(void){
 
 	get_duty_value();
@@ -160,8 +155,7 @@ void update_1khz(void){
 
 }
 
-=======
->>>>>>> 0d6478ce6432d025df209c0aa6b5735b1b51b05b
+
 float get_duty_value(){
 
 	if(HAL_ADC_Start_DMA(&hadc1, (uint32_t *)adc_values, 7)== HAL_OK){
@@ -190,11 +184,8 @@ void set_deadtime_pwm_ns(uint32_t time_ns){
 }
 
 void set_duty_pwm(float duty){
-<<<<<<< HEAD
 
 	LL_TIM_DisableAllOutputs(TIM1);
-=======
->>>>>>> 0d6478ce6432d025df209c0aa6b5735b1b51b05b
 	TIM1->CCR1 = duty ;
 	TIM1->CCR2 = duty ;
 	TIM1->CCR3 = duty ;
